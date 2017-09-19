5bff16804acd11e93ff24238421c889357309c0c
anupvkanale <anupvkanale@gmail.com>
2017-09-19 10:50:41 -0700

    intro to mpi assignment

---
+++ Assignment2/Codes/global_driver.c
@@ -0,0 +1,50 @@
+#include "mpi.h"
+#include <stdio.h>
+int nprocs;  /* Number of processors */
+int myid;    /* My rank */
+
+double global_sum(double partial) {
+  /* Implement your own global summation here */
+	double partial, hisdone, mydone, partner;
+	int bitValue;
+	
+  mydone = partial;
+  for bitValue=1, bitValue<nprocs; bitValue*=2{
+		//partner := myid XOR 2-to-the-power-l;
+  	partner=myid^bitValue;
+    //send mydone to partner;
+		MPI_Send(&mydone,1,MPI_DOUBLE,partner,bitValue,MPI_COMM_WORLD);
+    //receive hisdone from partner;
+		MPI_Recv(&hisdone,1,MPI_DOUBLE,partner,bitValue,MPI_COMM_WORLD, &status);
+    mydone = mydone + hisdone;
+  }
+  return mydone;
+}
+
+int main(int argc, char *argv[]) {
+  double partial, sum, avg;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  partial = (double) myid;
+  printf("Node %d has %le\n", myid, partial);
+
+  sum = global_sum(partial);
+
+  if (myid == 0) {
+    avg = sum/nprocs;
+    printf("Global average = %le\n", avg);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+
+cd /home/rcf-proj/an2/kanale/
+mpicc -o global global.c -lm
+
+then use global.pbs
